113f66e1b65ae424b0af501c25ea38242615cfa3
= <george.bentes@gmail.com>
2023-05-20 00:58:09 -0300

    Add core filter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { TitleComponent } from './components/title/title.component';
 import { UniverseComponent } from './pages/universe/universe.component';
+import { FilterComponent } from './components/filter/filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TitleComponent,
     UniverseComponent,
+    FilterComponent,
   ],
   imports: [
     BrowserModule,
@@ -21,6 +25,8 @@ import { UniverseComponent } from './pages/universe/universe.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatGridListModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
