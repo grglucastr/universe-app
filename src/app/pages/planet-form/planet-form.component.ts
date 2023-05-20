846a1b530cc13536803bba886694a067d6c8da17
= <george.bentes@gmail.com>
2023-05-20 01:29:58 -0300

    Add planet page

---
+++ src/app/pages/planet-form/planet-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-planet-form',
+  templateUrl: './planet-form.component.html',
+  styleUrls: ['./planet-form.component.css']
+})
+export class PlanetFormComponent {
+
+}
